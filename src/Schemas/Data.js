c5fbcbc953d241bcea67e79c6babccd12334a051
maldisou42 <maldisou@student.42abudhabi.ae>
2022-12-05 15:36:54 +0400

    1st

---
+++ src/Schemas/Data.js
@@ -0,0 +1,205 @@
+export const Data = {
+  profile: {
+    name: "Mohammed Disougi",
+    ocupation: "Bsc Hon Electronics Engineering | Nominated Talent for Golden Visa | Business analysts & developer | Product Manager | Research & Innovations | Climate Change & Renewable energy driven leader | Event Coordinator |42 Abu Dhabi Student",
+    location: "United Arab  Emirates, Abu Dhabi",
+    email: "mohammedaldisougi@gmail.com | maldisou@student.42abudhabi.ae",
+    telephone: "+971 585307008",
+    image: "images/personal_photo.jpg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+      " Professional business analyst, programmer Skilled technical analyst with in-depth knowledge of Tech, Industrial Automation Systems, Fire Alarms Systems, industrial Drones, UAVs, and UAS. Quality-driven leader Dedicated to progress, Self-motivated individual who seizes numerous opportunities for corporate expansion and innovation, Capability to take on responsibilities and perform with little or no supervision. I am determined and highly excited about Drones, potential for expansion and innovation in company, and I have good judgment, initiative, passion, and resourcefulness. Experience in boosting earnings, lowering costs, and altering customer service standards. Attendance at most major events, conferences, and exhibitions linked to automation, airports, business, cyber security and IT, and security. Experienced in leading and directing operational teams. Excellent verbal and written communication abilities",
+  },
+  skills: {
+    technicalLabel: "Technology",
+    softLabel: "Skills",
+    technicalSkills: [
+      "C",
+      "Shell",
+      "VIM",
+      "Virtual Machines",
+      "ReactJS",
+      "wordpress",
+      "Python",
+      "PLC",
+      "Robotics",
+      "Microsoft Excel",
+      "Visual Stuido",
+      "DOCKER",
+      "ARDUINO",
+      "MYSQL",
+      "Git",
+    ],
+    softSkills: [
+      "opertation management",
+      "Analytical capacity",
+      "attentive to details",
+      "Problem Solving",
+      "Critical Thinkning",
+      "Event Managment",
+      "Artificial Intelligence",
+      "Stakeholder Engagement",
+      "Business Development",
+      "Communication",
+      "Logistics Management",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Maldisou",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/mohammed-disougi-9427a9135/",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "Maldisou",
+        name: "Gmail",
+        url: "https://mohammedaldisougi@gmail.com/",
+        className: "bxl-gmail",
+      },
+    ],
+  },
+
+  experience: {
+    works: [
+      {
+        title: "Operation Manager | Zone Manager",
+        period: "Nov. 2022 - Dec 2022 ",
+        company: "ARGA",
+        description: [
+          "World Tri-Athlon Abu Dhabi 2022","FORMULA 1 ETIHAD AIRWAYS ABU DHABI GRAND PRIX 2022"
+        ],
+      },
+      
+      {
+        title: "Data Analyst",
+        period: "Jan. 2021 - Feb. 2022",
+        company: "Royal Health Group",
+        description: [
+          "Analysing and collecting data using various statiscal tools.",
+          "Contribute to market research, identifying market potential and find opportunities for growth.",
+          "",
+          "",
+          "Guiding the development team, creating Dashboards, flowcharts, and graphs",
+        ],
+      },
+       {
+        title: "Business Analyst",
+        period: "Nov. 2019 - Aug. 2020",
+        company: "Multiline tech L.L.C Trading",
+        description: [
+          "Determined growth opportunities to ensure efficient sales strategies down the line with the management's vision",
+          "Responding to clients & prospects inquires and assessing client requirements.",
+          "Lead strategic account development plans, maximizing development and growth opportunities.",
+          "building strategic relationships within the aviation MRO industry using Odoo CRM managing the procurement of spare parts. Generating new business leads to further building up and managing own key accounts. Supporting colleagues Liaising with other departments, such as Technical Operations.."
+        ],
+      },
+      {
+        title: "Instrument Control Engineer",
+        period: "Feb. 2017 - Feb. 2020",
+        company: "ARABIAN POWER COMPANY",
+        description: [
+          "Maintain all safety measure",
+          "Charged with installation, commissioning of Instruments, sensor such as RTDs, Thermocouples, Pressure",
+          "transmitters ,PH , Conductivities Oxygen analyzers ,etc.",
+          "Guided the final inspection and commissioning of the I&C related systems and equipment’s such as Pneumatic Control System ,Hydraulic control system, Fire alarm system, Emission Monitoring System. Performed conceptual, preliminary and detailed design of I&C systems and modifications, including definition of system functional and performance requirements and control algorithms, definition of control logic, control parameters, set points, instrument ranges and environmental and seismic qualification.",
+          "Support to credit management and finance in case of disputes and credit collection problems.",
+          ],
+      },
+      
+      {
+        title: "Associate Research & Development | Electronics Engineer",
+        period: "jun. 2016 - Current",
+        company: "freelance ",
+        description: [
+          "Researching on industrial, UAVs, and UAS ",
+          "building, developing programs in ardiuno  ",
+          "Debbuging and testing performances of Programs"
+          ],
+      },
+      {
+        title: "Event managment Specialist",
+        period: "Aug. 2010 ",
+        company: "freelancer | Abu Dhabi, UAE",
+        description: [
+          //"Clients Collaboration: Flash Entertainment, WOW event, Entourage "
+          ],
+      },
+    ],
+    academic: [
+      {
+        career: "Product Manager | Abu Dhabi, UAE ",
+        date: "Sep. 2022",
+        institution: "Udacity - Nano Degree",
+      },
+      {
+        career: "Business Analyst | Abu Dhabi, UAE ",
+        date: "feb. 2022",
+        institution: "Udacity - Nano Degree",
+      },
+      {
+        career: "Climate Change & Energy Transition | Abu Dhabi, UAE ",
+        date: "Jul. 2022",
+        institution: "Total Energies | Sorbone University  ",
+      },
+      {
+        career: "AI Robotics Course | Abu Dhabi, UAE ",
+        date: "Aug. 2022",
+        institution: "DataTech Labs - AWS Partner ",
+      },
+      {
+        career: "Software Developments | Abu Dhabi, UAE ",
+        date: "jun. 2021",
+        institution: "42 Abu Dhabi",
+      },
+      {
+        career: "B.Sc. (Hons) in Electronics Control Engineering | khartoum, Sudan",
+        date: "2011 - 2016",
+        institution: "University of Medical Science & Technology",
+      },
+      {
+        career: "High School Diploma | Abu Dhabi, UAE ",
+        date: "2011",
+        institution: "AL Mutannabi Secondary School",
+      }
+    ],
+    proyects: [
+      {
+        name: "ENTREPRENEURSHIP",
+        company: "",
+        period: "",
+        description: [
+          "",
+        ],
+      },
+      {
+        name: "WEB DEVELOPMENT",
+        company: " ",
+        period: "",
+        description: [
+          "",
+        ],
+      },
+      {
+        name: "READING",
+        company: "",
+        period: "",
+        description: [
+          "",
+        ],
+      },
+      {
+        name: "BASKETBALL",
+        company: "",
+        period: "",
+        description: [
+          "",
+        ],
+      },
+    ],
+  },
+};
